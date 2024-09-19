682c5314482f6a7adf86783766baffb2327bf6d7
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-09-19 11:20:23 -0400

    Tells weather

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Rebecca de Boer
+ * Created on: Sep 2024
+ * This program Tells tempeture
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+
+let tempeture = input.temperature()
+
+input.onButtonPressed(Button.A, function () {
+    tempeture = input.temperature()
+    basic.showNumber(tempeture)
+})
